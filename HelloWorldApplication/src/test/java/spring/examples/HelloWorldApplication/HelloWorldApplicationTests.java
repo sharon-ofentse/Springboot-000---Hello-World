86ad0ed8b50b4e8ec7d41739613b7ba6f9f5d13f
Gabanakgosi Sharon <sgabanakgosi@enfint.tech>
2025-03-16 17:49:46 +0200

    Add HelloWorldApplication as a regular folder

---
+++ HelloWorldApplication/src/test/java/spring/examples/HelloWorldApplication/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.examples.HelloWorldApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
